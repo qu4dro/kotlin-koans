50704bf30feb55070105b3a5ce67be886c85a7bf
qu4dro <33917440+qu4dro@users.noreply.github.com>
2021-01-13 11:03:15 +0800

    Nullable types task completed

--- Introduction/Nullable types/src/Task.kt
+++ Introduction/Nullable types/src/Task.kt
@@ -1,7 +1,12 @@
 fun sendMessageToClient(
-        client: Client?, message: String?, mailer: Mailer
+    client: Client?, message: String?, mailer: Mailer
 ) {
-    TODO()
+    val personalInfo: PersonalInfo? = client?.personalInfo
+    val email: String? = personalInfo?.email
+    if (email != null && message != null) {
+        mailer.sendMessage(email, message)
+    }
+
 }
 
 class Client(val personalInfo: PersonalInfo?)
