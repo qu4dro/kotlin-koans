add57d44248b98fb8b47d009ed2361660740fe73
qu4dro <33917440+qu4dro@users.noreply.github.com>
2021-01-14 15:57:46 +0800

    Rename on import task completed

--- Classes/Sealed classes/src/Task.kt
+++ Classes/Sealed classes/src/Task.kt
@@ -1,9 +1,9 @@
 fun eval(expr: Expr): Int =
-        when (expr) {
-            is Num -> expr.value
-            is Sum -> eval(expr.left) + eval(expr.right)
-        }
+    when (expr) {
+        is Num -> expr.value
+        is Sum -> eval(expr.left) + eval(expr.right)
+    }
 
 sealed class Expr
-    data class Num(val value: Int) : Expr()
-    data class Sum(val left: Expr, val right: Expr) : Expr()
+data class Num(val value: Int) : Expr()
+data class Sum(val left: Expr, val right: Expr) : Expr()
