765257efde843d029cfe28447bbc13fd0843a504
qu4dro <33917440+qu4dro@users.noreply.github.com>
2021-02-08 11:23:09 +0800

    Comparison task completed

--- Conventions/Comparison/src/Task.kt
+++ Conventions/Comparison/src/Task.kt
@@ -1,5 +1,12 @@
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override operator fun compareTo(other: MyDate): Int {
+        if (this.year > other.year || this.month > other.year || this.dayOfMonth > other.dayOfMonth) return 1
+        return if (this.year < other.year || this.month < other.year || this.dayOfMonth < other.dayOfMonth) -1
+        else 0
+    }
 }
+
+
 
 fun test(date1: MyDate, date2: MyDate) {
     // this code should compile:
