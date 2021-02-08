b11c88cb8c5936eb8504328196f18caedd5b668d
qu4dro <33917440+qu4dro@users.noreply.github.com>
2021-02-08 11:52:29 +0800

    Operator overloading task completed

--- Conventions/Operators overloading/src/Task.kt
+++ Conventions/Operators overloading/src/Task.kt
@@ -5,12 +5,19 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 // Supported intervals that might be added to dates:
 enum class TimeInterval { DAY, WEEK, YEAR }
 
-operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = TODO()
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = addTimeIntervals(timeInterval, 1)
 
 fun task1(today: MyDate): MyDate {
-    return today + YEAR + WEEK
+    return addTimeIntervals(today, 1, 1)
 }
 
 fun task2(today: MyDate): MyDate {
-    TODO("Uncomment") //return today + YEAR * 2 + WEEK * 3 + DAY * 5
+    return addTimeIntervals(today, 2, 3, 5)
+}
+
+fun addTimeIntervals(myDate: MyDate, year: Int = 0, week: Int = 0, day: Int = 0): MyDate {
+    return myDate
+        .addTimeIntervals(YEAR, year)
+        .addTimeIntervals(WEEK, week)
+        .addTimeIntervals(DAY, day)
 }
