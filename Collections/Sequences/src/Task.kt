b8cc36bb7bc353494222f4e26be19a4585a6e146
qu4dro <33917440+qu4dro@users.noreply.github.com>
2021-02-11 11:09:43 +0800

    Sequences task completed

--- Collections/Sequences/src/Task.kt
+++ Collections/Sequences/src/Task.kt
@@ -1,14 +1,14 @@
 // Find the most expensive product among all the delivered products
 // ordered by the customer. Use `Order.isDelivered` flag.
 fun findMostExpensiveProductBy(customer: Customer): Product? {
-    TODO()
+    return customer.getOrderedProducts().maxBy { it.price }
 }
 
 // Count the amount of times a product was ordered.
 // Note that a customer may order the same product several times.
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
-    TODO()
+    return customers.flatMap { it.orders }.flatMap { it.products }.count { it == product }
 }
 
 fun Customer.getOrderedProducts(): Sequence<Product> =
-        TODO()
+    orders.filter { it.isDelivered }.flatMap { it.products }.asSequence()
