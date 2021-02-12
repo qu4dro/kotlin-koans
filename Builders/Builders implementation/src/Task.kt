7ebbed76e302afd39eb2e8bc3bb4b17516205443
qu4dro <33917440+qu4dro@users.noreply.github.com>
2021-02-12 14:29:57 +0800

    Builders implementation task completed

--- Builders/Builders implementation/src/Task.kt
+++ Builders/Builders implementation/src/Task.kt
@@ -13,13 +13,15 @@ fun table(init: TABLE.() -> Unit): TABLE {
 
 class TABLE : Tag("table") {
     fun tr(init: TR.() -> Unit) {
-        /* TODO */
+        val tr = TR()
+        tr.init()
+        children += tr
     }
 }
 
 class TR : Tag("tr") {
     fun td(init: TD.() -> Unit) {
-        /* TODO */
+        children += TD().apply(init)
     }
 }
 
